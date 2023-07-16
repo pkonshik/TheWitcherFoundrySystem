d5f8027030fa41352581263718c1349a4e29f4d1
pkonshik <prosh.konshik@gmail.com>
2023-07-16 23:15:49 +0300

    Initial commit: added .editor, .gitattributes modified .gitignore
    added assets, lang, css, js, db files

---
+++ module/settings.js
@@ -0,0 +1,65 @@
+export const registerSettings = function() {
+    // Register any custom system settings here
+    game.settings.register("witcher", "useOptionnalAdrenaline", {
+        name: "WITCHER.Settings.Adrenaline",
+        hint: "WITCHER.Settings.AdrenalineDetails",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "displayRollsDetails", {
+        name: "WITCHER.Settings.displayRollDetails",
+        hint: "WITCHER.Settings.displayRollDetailsHint",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "useWitcherFont", {
+        name: "WITCHER.Settings.specialFont",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "displayRep", {
+        name: "WITCHER.Settings.displayReputation",
+        hint: "WITCHER.Settings.displayReputationHint",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "useOptionnalVerbalCombat", {
+        name: "WITCHER.Settings.useVerbalCombatRule",
+        hint: "WITCHER.Settings.useVerbalCombatRuleHint",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "loadCustomStatusesFromCompendium", {
+        name: "WITCHER.Settings.loadCustomStatusesFromCompendium",
+        hint: "WITCHER.Settings.loadCustomStatusesFromCompendiumHint",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+      });
+    game.settings.register("witcher", "clickableImageItemTypes", {
+        name: "WITCHER.Settings.clickableImageItemTypes",
+        hint: "WITCHER.Settings.clickableImageItemTypesHint",
+        scope: "world",
+        config: true,
+        type: String,
+        default: "valuable"
+      });
+    game.settings.register("witcher", "clickableImageCheckboxForGMOnly", {
+        name: "WITCHER.Settings.clickableImageCheckboxForGMOnly",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+      });
+}
